7c465a1f4444878e5def2273fe517f64a255fdc5
Talay <seyfullah.talay@mannheim.de>
2023-04-19 16:29:37 +0200

    templating und landing page redesign

---
+++ bits/footer.php
@@ -0,0 +1,10 @@
+<footer class="text-muted py-5">
+        <div class="container">
+            <p class="float-end mb-1">
+                <a href="#top">Back to top</a>
+            </p>
+            <p class="mb-1">
+                Copyright © Amogus inc. 2027
+            </p>
+        </div>
+    </footer>
---
+++ bits/header.php
@@ -0,0 +1,34 @@
+<header id="top">
+        <div class="collapse bg-light" id="navbarHeader">
+            <div class="container">
+                <div class="row">
+                    <div class="col-sm-8 col-md-7 py-4">
+                        <h4>BonoBros</h4>
+                        <p class="text-muted">
+                            Random Informationstext.
+                        </p>
+                    </div>
+                    <div class="col-sm-4 offset-md-1 py-4">
+                        <h4 class="text-white">Contact</h4>
+                        <ul class="list-unstyled">
+                            <li><a href="#" class="text-white">Follow on Twitter</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <nav class="navbar navbar-light shadow-sm">
+            <div class="container">
+                <a href="#" class="navbar-brand d-flex align-items-center">
+                    <img src="img/Affe.png" width="145" height="120" alt="">
+                    <img src="img/BonoBrosLogo.png" width="280" height="120" alt="Logo">
+                    <strong>Affenmäßig stark!</strong>
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarHeader"
+                    aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+        </nav>
+    </header>
