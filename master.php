c794374aec25ad7e3f02ba50ab2258f998eee0c1
KingSeyfo <seyfullahtalay@outlook.de>
2023-04-19 19:54:27 +0200

    erstes erfolgreiches Templating

--- master.php
+++ master.php
@@ -1,7 +1,17 @@
-<?php 
-include_once("header.php");
-
-echo $pageContent;
-
-include_once("footer.php");
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php echo $pageLinks?>
+    <title><?php echo $pageTitle?></title>
+</head>
+<body>
+    <?php 
+    include_once("bits/header.php");
+    echo $pageContent;
+    include_once("bits/footer.php");
+    ?>
+</body>
+</html>
