c2e3a1d5566b26d7846f71d0e733195636dad036
KingSeyfo <seyfullahtalay@outlook.de>
2023-04-21 00:22:00 +0200

    ka

--- login.php
+++ login.php
@@ -2,7 +2,6 @@
 $pageStyles = '<link rel="stylesheet" href="css/login.css">';
 $pageTitle = "Login";
 $pageContent = file_get_contents("content/login.html");
-    
     include_once("master.php"); 
     
 ?>
