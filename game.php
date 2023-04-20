3f846b35728e913340a1020bb43df6b8a354d568
KingSeyfo <seyfullahtalay@outlook.de>
2023-04-20 19:51:48 +0200

    CSS Files besser sortiert und besser strukturiert

--- game.php
+++ game.php
@@ -1,6 +1,6 @@
 <?php
 $gamename = $_GET['gamename'];
-// check if game exists {
+// check if game exists { 
     //variablen setzen
 include_once("master.php");
 //}
--- landingpage.php
+++ landingpage.php
@@ -1,54 +1,7 @@
 <?php
 $pageStyles = '<link rel="stylesheet" href="/css/landingpage.css">';
 $pageTitle = "Home";
-$pageContent = '<main>
-        <div class="px-md-5 mx-md-5 album py-5 bg-light">
-            <div class="container">
-                <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                    <div class="col">
-                        <a href="/game.php?gamename=fortnite" class="card shadow-sm">
-                            <img src="img/Fortnite.jpg" alt="">
-                            <div class="card-body">
-                                <p class="card-text">
-                                    Fortnite
-                                </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted">9926 Spieler</small>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col">
-                        <a href="/game.php?gamename=amogus" class="card shadow-sm">
-                            <img src="img/Amogus.jpg" alt="">
-                            <div class="card-body">
-                                <p class="card-text">
-                                    Amogus
-                                </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted">9926 Spieler</small>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-                    <div class="col">
-                        <a href="/game.php?gamename=mario" class="card shadow-sm">
-                            <img src="img/Mario.jpg" alt="">
-                            <div class="card-body">
-                                <p class="card-text">
-                                    Mario
-                                </p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                    <small class="text-muted">9926 Spieler</small>
-                                </div>
-                            </div>
-                        </a>
-                    </div>
-
-                </div>
-            </div>
-        </div>
-    </main>';
+$pageContent = file_get_contents("content/landingpage.html");
 
 include_once("master.php");
 ?>
