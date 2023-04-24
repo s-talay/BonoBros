b3acdca2702613b7c3fcc15cbc0a3a38d4c6d23c
Talay <seyfullah.talay@mannheim.de>
2023-04-24 10:24:11 +0200

    Login Datenbank aufruf

---
+++ bits/navslideLogged.php
@@ -0,0 +1,17 @@
+<div class="collapse bg-light" id="navbarHeader">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-8 col-md-7 py-4">
+                <h4>BonoBros</h4>
+                <p class="text-muted">
+                    Random Informationstext.
+                </p>
+            </div>
+            <div class="col-sm-4 offset-md-1 py-4">
+                <ul class="list-unstyled">
+                    <li><a href="/login.php">Logged In</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
--- login.php
+++ login.php
@@ -3,5 +3,47 @@ $pageStyles = '<link rel="stylesheet" href="css/login.css">';
 $pageTitle = "Login";
 $pageContent = file_get_contents("content/login.html");
     include_once("master.php"); 
-    
+
+$sname= "localhost";
+
+$unmae= "root";
+
+$password = "";
+
+$db_name = "test";
+
+$conn = mysqli_connect($sname, $unmae, $password, $db_name);
+
+if (!$conn) {
+    echo "Connection failed!";
+}
+session_start();
+if(isset($_POST["email"]) && isset($_POST["password"])){
+    function validate($data){
+
+        $data = trim($data);
+ 
+        $data = stripslashes($data);
+ 
+        $data = htmlspecialchars($data);
+ 
+        return $data;
+ 
+     }
+     $email = validate($_POST["email"]);
+     $password = validate($_POST["password"]);
+
+     $sql = "SELECT * FROM `login test` WHERE email='$email' AND password='$password';";
+     $result = mysqli_query($conn, $sql);
+     if(mysqli_num_rows($result) === 1){
+        $row = mysqli_fetch_assoc($result);
+        if($row["email"] == $email && $row["password"] == $password){
+            echo "Logged in!";
+            $_SESSION["email"] = $row["email"];
+            $_SESSION["password"] = $row["password"];
+            header("Location: landingpage.php");
+            exit();
+        }
+     }
+}
 ?>
