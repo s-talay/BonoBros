525372852af91d579ed1cba40ebd692e1847976e
KingSeyfo <seyfullahtalay@outlook.de>
2023-04-24 20:42:31 +0200

    php halbherzig eingefügt, muss noch gefixxt werden, email hinzufügen etc

---
+++ register.php
@@ -0,0 +1,6 @@
+<?php
+$pageStyles = '<link rel="stylesheet" href="css/register.css">';
+$pageTitle = "Registrieren";
+$pageContent = file_get_contents("content/register.html");
+    include_once("master.php"); 
+?>
