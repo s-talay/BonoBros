9933d26d763048d40e4d51a30db762099a26dbab
Talay <seyfullah.talay@mannheim.de>
2023-05-03 12:21:20 +0200

    SQL bissl gefixt

--- register.php
+++ register.php
@@ -6,15 +6,23 @@ $pageContent = file_get_contents("content/register.php");
     include_once("master.php"); 
 ?>
 <?php
+function debug_to_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
 // Include config file
 require_once "config.php";
  
 // Define variables and initialize with empty values
-$email = $username = $password = $password_repeat = "";
-$email_err = $username_err = $password_err = $password_repeat_err = "";
+$email = $username = $password = $confirm_password = "";
+$email_err = $username_err = $password_err = $confirm_password_err = "";
  
 // Processing form data when form is submitted
 if($_SERVER["REQUEST_METHOD"] == "POST"){
+    debug_to_console("POST");
  
     // Validate username
     if(empty(trim($_POST["username"]))){
@@ -50,6 +58,39 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $stmt->close();
         }
     }
+
+    // Validate email
+    if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter your email address.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE email = ?";
+        
+        if($stmt = $mysqli->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("s", $param_email);
+            
+            // Set parameters
+            $param_email = trim($_POST["email"]);
+            
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // store result
+                $stmt->store_result();
+                
+                if($stmt->num_rows == 1){
+                    $email_err = "This E-Mail has already been used.";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            $stmt->close();
+        }
+    }
     
     // Validate password
     if(empty(trim($_POST["password"]))){
@@ -69,26 +110,32 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $confirm_password_err = "Password did not match.";
         }
     }
-    
+    debug_to_console($email_err);
+    debug_to_console($username_err);
+    debug_to_console($password_err);
+    debug_to_console($confirm_password_err);
     // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){
+    if(empty($username_err) && empty($email) && empty($password_err) && empty($confirm_password_err)){
         
         // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+        $sql = "INSERT INTO users (email, username, password) VALUES (?, ?, ?)";
          
         if($stmt = $mysqli->prepare($sql)){
             // Bind variables to the prepared statement as parameters
-            $stmt->bind_param("ss", $param_username, $param_password);
+            $stmt->bind_param("sss", $param_email, $param_username, $param_password);
             
             // Set parameters
+            $param_email = $email;
             $param_username = $username;
             $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
             
             // Attempt to execute the prepared statement
             if($stmt->execute()){
+                debug_to_console("1");
                 // Redirect to login page
-                header("location: login.php");
+                // header("location: login.php");
             } else{
+                debug_to_console("2");
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
