1b050ec6f227da0d9bc8a7c978718ff1e884df5f
KingSeyfo <seyfullahtalay@outlook.de>
2023-05-17 16:31:11 +0200

    neu organisiert

--- php/master.php
+++ php/master.php
@@ -23,6 +23,7 @@ function logout()
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="/img/favicon.ico">
     <base href="/">
     <?php
     include_once($root."/bits/stylelinks.php");
