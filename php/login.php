e7721593dc8d96e9f51ba1b09696b5e3bc752dcf
seyfu <seyfullahtalay@outlook.de>
2023-05-31 14:30:10 +0200

    asdasd

--- php/login.php
+++ php/login.php
@@ -66,6 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
+                            
 
                             // Redirect user to welcome page
                             header("location: landingpage.php");
