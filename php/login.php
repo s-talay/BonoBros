5d4f92987c304059c6159316cfd8135694e0f672
seyfu <seyfullahtalay@outlook.de>
2023-05-31 14:32:51 +0200

    variablen bei sql abfrage

--- php/login.php
+++ php/login.php
@@ -40,7 +40,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate credentials
     if (empty($username_err) && empty($password_err)) {
         // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
+        $sql = "SELECT id, username, password, admin, enabled FROM users WHERE username = ?";
 
         if ($stmt = $mysqli->prepare($sql)) {
             // Bind variables to the prepared statement as parameters
@@ -66,6 +66,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;
+                            
 
                             // Redirect user to welcome page
                             header("location: landingpage.php");
