54e62b1f3160cf86ccf540630347d9d0ec86c9d4
KingSeyfo <seyfullahtalay@outlook.de>
2023-06-11 13:33:09 +0200

    php.ini

--- config.php
+++ config.php
@@ -1,20 +1,14 @@
 <?php
-/* Database credentials. Assuming you are running MySQL
-server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'bonobros');
- 
-/* Attempt to connect to MySQL database */
+$ini_path = "php.ini";
+$array = parse_ini_file($ini_path);
+define('DB_SERVER', $array["db_host"]);
+define('DB_USERNAME', $array["db_username"]);
+define('DB_PASSWORD', $array["db_password"]);
+define('DB_NAME', $array["db_name"]);
 
 $mysqli = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 if($mysqli === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
 }
-
-
- 
-
 ?>
